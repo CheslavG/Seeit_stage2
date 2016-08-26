d4c244bd05796f41d0ca49fb900436ee14104166
cheslavg <cheslavg@gmail.com>
2016-08-26 11:51:43 +0300

    Fully ready showing and clipping stage

--- app/src/main/java/cheslavg/seeit/MainActivity.java
+++ app/src/main/java/cheslavg/seeit/MainActivity.java
@@ -6,6 +6,7 @@ import android.graphics.drawable.ClipDrawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -26,10 +27,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     ColorMatrix matrix = new ColorMatrix();
     ImageView imageViewClip;
     int level_delta = 10000;
+    private static final String TAG = AppCompatActivity.class.getSimpleName();
 
 
 
-    @Override
+        @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         //OnCreate and other standart stuff
@@ -39,36 +41,44 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         //find all relatives for Android Studio
 
         //first our images
+        Log.d(TAG,"-"+"find our images");
         imageViewGrayScale = (ImageView)findViewById(R.id.imageViewGrayScale);
         imageViewClip = (ImageView) findViewById(R.id.imageViewClip);
 
         //than out EditTexts
+        Log.d(TAG,"-"+"find out EditTexts");
         etSetAim = (EditText)findViewById(R.id.etSetAim);
         etSetMax = (EditText)findViewById(R.id.etSetMax);
         etSetStep = (EditText)findViewById(R.id.etSetStep);
 
         //than our TextViews
+        Log.d(TAG,"-"+"find out TextViews");
         tvAim = (TextView)findViewById(R.id.tvAim);
         tvMax = (TextView)findViewById(R.id.tvMax);
 
         //and than our alone button
+
+        Log.d(TAG,"-"+"find out SetButton");
         SetBtn = (Button)findViewById(R.id.SetBtn);
 
         //Now we set up our listeners
+
+        Log.d(TAG,"-"+"set listener to the SetButton");
         SetBtn.setOnClickListener(this);
         //etSetMax.setOnEditorActionListener(this);
 
+
         //Than go to our inspirational pictures -- grayscale
+
+        Log.d(TAG,"-"+"create grayscale filter");
         matrix.setSaturation(0);
         ColorMatrixColorFilter filter = new ColorMatrixColorFilter(matrix);
+        Log.d(TAG,"-"+"set grayscale filter to ImageViewGrayScale");
         imageViewGrayScale.setColorFilter(filter);
 
         //And than -- our Clipped Drawable
+        Log.d(TAG,"-"+"create clipDrawable from the ImageViewClip");
         clipDrawable = (ClipDrawable) imageViewClip.getDrawable();
-
-        //
-        //String aim = etSetAim.getText().toString();
-        //String aim_max = etSetMax.getText().toString();
 
 
 
@@ -77,6 +87,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.SetBtn:
+                Log.d(TAG,"-"+"Check if you fields are not empty");
                 if(TextUtils.isEmpty(etSetMax.getText().toString())||
                         TextUtils.isEmpty(etSetAim.getText().toString())
                         ||TextUtils.isEmpty(etSetStep.getText().toString()))
@@ -84,6 +95,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     break;
                 }
                 else {
+                Log.d(TAG,"-"+"Get the percentage of coloring throw clipDrawable.setLevel(level)");
                 int level = level_delta*Integer.parseInt(etSetStep.getText().toString())/Integer.parseInt(etSetMax.getText().toString());
 
                     clipDrawable.setLevel(level);
